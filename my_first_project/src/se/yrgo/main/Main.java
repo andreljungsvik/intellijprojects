c59f54ad5a2af8bb80352cacb44a8ffb28d88da9
andreljungsvik <140394392+andreljungsvik@users.noreply.github.com>
2024-10-29 08:30:45 +0100

    almost first commit XD

---
+++ my_first_project/src/se/yrgo/main/Main.java
@@ -0,0 +1,10 @@
+package se.yrgo.main;
+
+import se.yrgo.domain.Book;
+
+public class Main {
+    public static void main(String[] args) {
+        Book minBok = new Book("Lasse maja", 98);
+        System.out.println(minBok);
+    }
+}
